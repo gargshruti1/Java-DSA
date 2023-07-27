f7aacf2ade5f10b09a797084176048785112a9e4
gargshruti1 <gargshruti190@gmail.com>
2023-07-27 19:03:48 +0530

    first commit

---
+++ src/main/java/TREE/BinaryTreesYT.java
@@ -0,0 +1,17 @@
+package TREE;
+
+public class BinaryTreesYT {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data= data;
+            this.left= null;
+            this.right=null;
+        }
+    }
+    public static void main (String args[]){
+        int nodes[]={2,8,-1,4,-1,-1,7,9};
+    }
+}
